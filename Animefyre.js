6f9efe7112d15069c3c8466b2fea001f356ae6ce
alvici <spittime@ymail.com>
2019-07-10 17:14:19 +0700

    Update Animefyre.js

--- Animefyre.js
+++ Animefyre.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-a_to_ve[i].href="https://anmfyre.blogspot.com/p/landing.html?id="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="https://anime-fyre.blogspot.com/p/landing.html?id="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
